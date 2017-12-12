8636c37bfc585c525af5a5098c47aec08c4ebbbd
Anushri Adhia <anushri.adhia@gmail.com>
2017-12-12 18:49:15 -0500

    board structure for conway's game of life

---
+++ GameOfLife/GameStructure.hs
@@ -0,0 +1,32 @@
+module GameStructure(
+    State (..),
+    Point,
+    GameBoard,
+    initializeBoard,
+    setStates,
+    getStates,
+    getNeighbors
+)
+
+--are the import statements supposed to go in the module or in main file?
+import Data.List
+import Data.Array
+
+data State = alive | dead deriving (Eg, Show)
+type Point = (Int, Int)
+type GameBoard = Array Point State
+
+initializeBoard :: Int -> GameBoard
+initializeBoard (dim) = 
+    array dimension $ zip (range dimension) (repeat dead)
+    where dimension = ((0,0), (dim-1, dim-1))
+
+setStates :: GameBoard -> [(Point, State)] -> GameBoard
+setStates = (//)
+
+getStates :: GameBoard -> [Point] -> [State]
+getStates gboard points = map (gboard!) points
+
+getNeighbors :: GameBoard -> Point -> [Point]
+getNeighbors board point@(row,col) = 
+    filter (/= point) $ filter (inRange(bounds board)) [(nrow, ncol)| nrow <- [row-1..row+1], ncol <- [col-1..col+1]]
