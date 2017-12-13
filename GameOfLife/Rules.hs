e6faa9dc76804db5408039be9889065a01c92ed0
Anushri Adhia <anushri.adhia@gmail.com>
2017-12-12 21:23:01 -0500

    generating new boards

---
+++ GameOfLife/Rules.hs
@@ -0,0 +1,30 @@
+import GameStructre
+
+nextGameBoard :: GameBoard -> GameBoard
+nextGameBoard board = 
+    let
+        cells = range (bounds board)
+        findState wantedState points = do
+            pts <- points
+            let state = board ! pts
+            guard $ state == wantedState
+            return (pts)
+        livingCells = findState alive cells
+        deadCells = findState dead cells
+        livingNeighborsNum = length . findState alive . getNeighbors board
+        underPopulated = do
+            living <- livingCells
+            guard $ (livingNeighborsNum living) < 2
+            return (living, dead)
+        overPopulated = do
+            living <- livingCells
+            guard $ (livingNeighborsNum living) > 3
+            return (living, dead)
+        newBorn = do
+            died <= deadCells
+            guard $ (aliveCells died) == 3
+            return (died, alive)
+    in setStates board (concat [underPopulated, overPopulated, newBorn])
+
+
+    
